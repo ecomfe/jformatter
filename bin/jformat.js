49077f02bcb9158f47430c9efc75e5c5827b5e74
ishowshao <ishowshao@me.com>
2014-03-13 15:59:57 +0800

    jformat bin

---
+++ bin/jformat.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var jformatter = require('jformatter');
+
+function showUsage() {
+    console.log('Usage:');
+    console.log('   jfromat file.js');
+    console.log();
+    process.exit(1);
+}
+
+if (process.argv.length < 2) {
+    showUsage();
+}
+
+var targetFile = null;
+process.argv.forEach(function(arg, index) {
+    if (index === 2) {
+        targetFile = arg;
+    }
+});
+
+if (typeof targetFile !== 'string') {
+    console.log('Error: no input file.');
+    process.exit(1);
+}
+
+jformatter.formatFile(targetFile);
