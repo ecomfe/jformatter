b8c94329f3d4da4fa8de891f6195e9f5dc422ae6
ishowshao <ishowshao@me.com>
2014-03-21 18:18:13 +0800

    update case and case output

--- test/output.js
+++ test/output.js
@@ -1,20 +1,12 @@
 var obj = {
     IdeaName: {
-        exMaxLength: langUnit.UNIT_NAME_MAX_LENTH,
-        required: 1,
-        requiredErrorMessage: '创意名称不能为空',
-        exMaxLengthErrorMessage: '创意名称不能超过30个字符'
-    },
-    CateSelect: {
+        exMaxLength: langUnit.UNIT_NAME_MAX_LENTH,required: 1,requiredErrorMessage: '创意名称不能为空',exMaxLengthErrorMessage: '创意名称不能超过30个字符'
+    },CateSelect: {
         datasource: "@category"
-    },
-    'key': {
-
-    }
+    },'key': {}
 };
-
 if (true) {
-} else {
+} else  {
     Dialog.alert({
         title: '提示',content: args.content,type: 'success'
     }).on('ok', function () {
@@ -26,31 +18,28 @@ if (true) {
 
          var url = buildUrl('idea/list', urls.uid, 'idea', {'pid':urls.pid});
          locator.redirect(url);*/
-        //accountTree.refresh(0, 'user', true);
-    });
+    //accountTree.refresh(0, 'user', true);
+});
 }
-
-
-
 var f = 'a\
         //a\
         ';
-var p = +d ; //test
+var p = +d; //test
 d = d === "true" ? !0 : d === "false" ? !1 : d === "null" ? null : +d + "" === d ? +d : H.test(d) ? p.parseJSON(d) : d;
 d = d === "true" ? !0 : d === "false" ? !1 : d === "null" ? null : +2 + "" === d ? +2 : H.test(d) ? p.parseJSON(d) : d;
-
-var a = -1, b = 0x2, c = +3, /* test */ f = String('f'), g;
-
+var a = -1, b = 0x2, c = +3, /* test */f = String('f'), g;
 var cd = (0 > 1) ? 1 + 1 : 2 / 2;
 var cd2 = 0 > 1 ? ((((1 + 1)) + 1)) : (2 > 3 ? 2 : 3);
 var cd3 = 0 > 1 ? {
-    cd3: 3
+    cd3:3
 } : function (a, b, c) {
     return a.apply(([]), b);
 };
-
-(function(func){func(1)})(function(arg){console.log(arg)});
-
+(function (func) {
+    func(1)
+})(function (arg) {
+    console.log(arg)
+});
 /**
  * something
  * @param p1
@@ -62,15 +51,12 @@ function doSomething(p1, p2) {
 }
 
 var d = null, e = {};
-
 var fs = require('fs')
-
 fs.readFile('tokens.json', function (err, data) { //aa
     //ab
     if (err) { //bb
         throw err; //
     }
-
     //TODO:多个else if 会有多换行的bug
     //TODO:多个else if 会有多换行的bug
     if (fs) {
@@ -86,41 +72,32 @@ fs.readFile('tokens.json', function (err, data) { //aa
     } else if (a > b) {
         console.log('else');
     }
-
     do {
         doSomething();
     } while (a > b);
-
     if (fs) {
-    } else {
+    } else  {
     }
-
     /**
      *
      * @type {{key: string, key2: {key3: string}}}
      */
     var map = {
-        key: 'value',
-        key2: {
+        key: 'value', key2: {
             key3: 'k3'
         }
     };
-
     var obj = ({
         whatAFuck: 'wtf'
     });
-
     window.globalVar = 'global';
-
-    var f, g = func2(), h; //a11
-    var aa, ab = function () {
+    var f,g = func2(),h; //a11
+    var aa,ab = function () {
     };
-    var x, y = {
+    var x,y = {
         z: 'z'
     }, z;
-
     var ac = x > 0 ? 1 : 2;
-
     try {
         if (ac > 0) {
             console.log(x > 0 ? 1 : 2)
@@ -128,47 +105,50 @@ fs.readFile('tokens.json', function (err, data) { //aa
     } catch (e) {
         console.log(e);
     }
-
     var whatAFuck = {
-        method: x > 0 ? function () {
+        method:x > 0 ? function () {
             doSomething(this, 2);
         } : function () {
             doSomething()
-        },
-        method2: function () {
+        }, method2:function () {
             doSomething();
         }
     };
-
     var number = 0;
     var obj = {};
     var str = '';
-
     var array = [];
     var array2 = [1, 2, 3, '4', NaN, '5'];
     var array3 = [
-        'stringEl', 10000, false, null, undefined, NaN, {
-            key: 'value1'
-        }, [1, 2, 3, 4, '5'], [
-            'a', {
-                b: 'c'
+        'stringEl',
+        10000,
+        false,
+        null,
+        undefined,
+        NaN,
+        {
+            key:'value1'
+        },
+        [1, 2, 3, 4, '5'],
+        [
+            'a',
+            {
+                b:'c'
             }
-        ], func2(), function () {
-
-        }, func(), [1, 2, 3, 4, '5']
+        ],
+        func2(),
+        function () {
+        },
+        func(),
+        [1, 2, 3, 4, '5']
     ];
-
     var func = function (obj, callback) {
     };
-
     func({
-        key: 'value'
+        key:'value'
     }, function () {
-
     }, function () {
-
     });
-
     /**
      *
      * @param data
@@ -180,35 +160,27 @@ fs.readFile('tokens.json', function (err, data) { //aa
         array2.push(map.key);
         return data + 'func2 return';
     };
-
-    for (var i = 0; i < number; i++) {
+    for (var i = 0;i < number;i++) {
         str += 'str';
         if (array.length > 0) {
             obj['abc'] = 'abc';
             obj.def = 'def';
-
             break;
         }
-
         if (array.length > 0) {
             continue;
         }
-
         func();
-
         delete map.key;
     }
-
     while (number > 0) {
         func();
         func2();
     }
-
     if ((function () {
         doSomething(1, 2);
     })()) {
-
     }
-
     //assss
 });
+
