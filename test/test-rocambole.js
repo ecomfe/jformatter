8c3b8f225ae0fce24c3d2a2db5a6a7ee7a7097b3
ishowshao <ishowshao@me.com>
2014-07-07 16:33:00 +0800

    调研rocambole
    调研rocambole

---
+++ test/test-rocambole.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+var rocambole = require('rocambole');
+var ast = rocambole.parse(require('fs').readFileSync('case.js', 'utf-8'));
+console.log( ast);
+// to get a string representation of all tokens call toString()
+//console.log( ast.toString() );
