ad0e02db0441b69ec9945546a7fa9004ff130aa6
ishowshao <ishowshao@me.com>
2014-08-05 23:29:56 +0800

    test case
    准备编写自动测试脚本

--- test/test-rocambole.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var rocambole = require('rocambole');
-var ast = rocambole.parse(require('fs').readFileSync('case.js', 'utf-8'));
-console.log(ast.tokens);
-// to get a string representation of all tokens call toString()
-//console.log( ast.toString() );
