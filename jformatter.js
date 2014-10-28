6c25b30332f1b4609a7825ef6de5e0f3266fd754
ishowshao <ishowshao@me.com>
2014-10-28 14:38:24 +0800

    remove a config
    无关样式的配置项，不应该在这里，保持功能的单一性

--- jformatter.js
+++ jformatter.js
@@ -34,7 +34,6 @@
                 other: 1 //TODO
             },
             other: {
-                singleVar: true, //TODO config name singleVar ?
                 keepArraySingleLine: false //TODO default formatted array multi line
             }
         };
