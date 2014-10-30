c819d52c3e4ccdab2906258dd0598833aacedcb0
ishowshao <ishowshao@me.com>
2014-10-30 16:06:11 +0800

    update test file

--- test/auto-test.js
+++ test/auto-test.js
@@ -2,17 +2,18 @@ var jformatter = require('../jformatter.js');
 var fs = require('fs');
 var ROOT = require('path').dirname(__filename);
 
-//test for default code style
-fs.readdir(ROOT, function (err, files) {
+// test for default code style
+console.log('Testing code style cases ...\n');
+fs.readdir(ROOT + '/test.style/case', function (err, files) {
     var allPass = true;
     files.forEach(function (path) {
         pathArr = path.split('.');
         if (pathArr[0] === 'case' && pathArr[pathArr.length - 1] === 'js') {
-            pathArr[0] = 'expect';
-            var expect = ROOT + '/' + pathArr.join('.');
-            if (fs.existsSync(expect)) {
+            pathArr[0] = 'check';
+            var checkFile = ROOT + '/test.style/check/' + pathArr.join('.');
+            if (fs.existsSync(checkFile)) {
                 var formattedString = jformatter.formatFile(ROOT + '/' + path);
-                if (formattedString === fs.readFileSync(expect, 'utf-8')) {
+                if (formattedString === fs.readFileSync(checkFile, 'utf-8')) {
                     console.log(path + ' ... pass.');
                 } else {
                     allPass = false;
@@ -28,4 +29,4 @@ fs.readdir(ROOT, function (err, files) {
     }
 });
 
-//test for code style config
+// test for code style config
