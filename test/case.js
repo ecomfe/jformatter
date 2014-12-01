9a4810e63aa32375766ce1b7d6bf9f598d8f0c96
ishowshao <ishowshao@me.com>
2014-12-01 21:42:01 +0800

    singleQuotes

---
+++ test/case.js
@@ -0,0 +1,16 @@
+var str1 = "abc, 123";
+var str2 = "abc, \'123";
+var str3 = "abc, \"123";
+var str4 = "abc, '123";
+var str5 = "abc, \''123";
+var str6 = 'abc, \'\'123';
+
+var obj = {
+    "a-1": 1,
+    "a-\"1": 1,
+    "a-'1": 1,
+    "a-\'1": 1,
+    "'": 1,
+    "\"\\": 1,
+    "var": 1
+};
